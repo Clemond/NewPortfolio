0b6fc3ee40496d3f092f36176938a3cc16627806
Nicholas <quackshows11@gmail.com>
2026-02-25 20:18:06 +0100

    feat: create header component and header css

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="container">
+      <p>Hello, im a header</p>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { projects } from "../data/projects";
 import { ProjectCard } from "../components/ProjectCard";
+import Header from "../components/Header";
 
 export const Home: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-      {projects.map((proj) => (
-        <ProjectCard key={proj.title} project={proj} />
-      ))}
+    <div>
+      <Header />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+        {projects.map((proj) => (
+          <ProjectCard key={proj.title} project={proj} />
+        ))}
+      </div>
     </div>
   );
 };
