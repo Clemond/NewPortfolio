7a24706cbb2fab32935ea9b6458e20ae145ea63a
Nicholas <quackshows11@gmail.com>
2026-02-26 15:42:13 +0100

    feat: add PresentationCard component and css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { projects } from "../data/projects";
 import { ProjectCard } from "../components/ProjectCard";
 import Header from "../components/Header/Header";
+import PresentationCard from "../components/PresentationCard";
 
 export const Home: React.FC = () => {
   return (
     <div>
       <Header />
+      <PresentationCard />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
         {projects.map((proj) => (
           <ProjectCard key={proj.title} project={proj} />
