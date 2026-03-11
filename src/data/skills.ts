bdafd4ec14d84b4a20246b735800ea29e0c49b3d
Nicholas <quackshows11@gmail.com>
2026-03-11 14:18:31 +0100

    feat: add and categorise all skills in a data file

---
+++ src/data/skills.ts
@@ -0,0 +1,20 @@
+export const skills: Record<string, string[]> = {
+  Frontend: [
+    "HTML",
+    "JavaScript",
+    "TypeScript",
+    "ReactJS",
+    "NextJS",
+    "Firebase"
+  ],
+  Native: ["React Native", "Kotlin", "Swift"],
+  StylingAndDesign: ["CSS", "TailwindCSS", "Figma", "UI/UX design process"],
+  Miscellaneous: [
+    "Git",
+    "Gitea",
+    "Agile methodology",
+    "Docker",
+    "REST APIs",
+    "PostGreSQL"
+  ]
+};
