c05e2f88290896799d75f718bd5f8df49e1f3f83
Nicholas <quackshows11@gmail.com>
2026-03-11 14:43:50 +0100

    fix: fix the skills data structure

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,20 +1,37 @@
-export const skills: Record<string, string[]> = {
-  Frontend: [
-    "HTML",
-    "JavaScript",
-    "TypeScript",
-    "ReactJS",
-    "NextJS",
-    "Firebase"
-  ],
-  Native: ["React Native", "Kotlin", "Swift"],
-  StylingAndDesign: ["CSS", "TailwindCSS", "Figma", "UI/UX design process"],
-  Miscellaneous: [
-    "Git",
-    "Gitea",
-    "Agile methodology",
-    "Docker",
-    "REST APIs",
-    "PostGreSQL"
-  ]
+type SkillCategory = {
+  title: string;
+  skills: string[];
 };
+
+export const skills: SkillCategory[] = [
+  {
+    title: "Frontend",
+    skills: [
+      "HTML",
+      "JavaScript",
+      "TypeScript",
+      "ReactJS",
+      "NextJS",
+      "Firebase"
+    ]
+  },
+  {
+    title: "Native",
+    skills: ["React Native", "Kotlin", "Swift"]
+  },
+  {
+    title: "Styling & Design",
+    skills: ["CSS", "TailwindCSS", "Figma", "UI/UX design process"]
+  },
+  {
+    title: "Miscellaneous",
+    skills: [
+      "Git",
+      "Gitea",
+      "Agile methodology",
+      "Docker",
+      "REST APIs",
+      "PostGreSQL"
+    ]
+  }
+];
