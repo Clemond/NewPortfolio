31f66d4ef6b64a4845781918edc3420ebcd75981
Nicholas <quackshows11@gmail.com>
2026-03-13 15:30:31 +0100

    feat: add AboutMeSection to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import Header from "../components/header/Header";
 import PresentationCard from "../components/intro-card/PresentationCard";
 import ProjectSection from "../components/project-section/ProjectSection";
 import SkillSection from "../components/skills-section/SkillSection";
+import AboutMeSection from "../components/about-me-section/AboutMeSection";
 
 export const Home: React.FC = () => {
   return (
     <div>
       <Header />
       <PresentationCard />
+      <AboutMeSection />
       <ProjectSection />
       <SkillSection />
     </div>
