886eb5d1ae42d293e6349edcd7c4b5097c29541d
Nicholas <quackshows11@gmail.com>
2026-03-18 21:31:20 +0100

    chore: In Header.css, change name of container to headerContainer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <div className="container">
+    <div className="headerContainer">
       <div className="nameField">
         <p className="name">Nicholas Nieminen Jönsson</p>
         <p className="jobTitle">iOS & Android Developer</p>
